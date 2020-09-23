8e947f7342337980f75310921693d8873c6b1772
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-09-23 10:04:59 +0200

    feat(leap): second iteration

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,8 +1,6 @@
 function isLeapYear(year: number): boolean {
-    const isDividableByFour = year % 4 === 0;
-    const isDividableByHundret = year % 100 === 0;
-    const isDividableByFouhhundret = year % 400 === 0;
-    return isDividableByFour && (!isDividableByHundret || isDividableByFouhhundret);
+    const isDividableBy = (divisor: number) => year % divisor === 0;
+    return isDividableBy(4) && (!isDividableBy(100) || isDividableBy(400));
 }
 
 export default isLeapYear
