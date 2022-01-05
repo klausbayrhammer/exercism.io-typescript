6e0fb6bbd3b5a1c403b5a8d38572c3673ae6a828
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2022-01-05 10:23:52 +0100

    Update etl.ts

--- etl/etl.ts
+++ etl/etl.ts
@@ -2,9 +2,10 @@ type LegacyScrabbleScores = Record<string, string[]>;
 type ScrabbleScores = Record<string, number>
 
 export function transform(legacyScrabbleScores: LegacyScrabbleScores): ScrabbleScores {
-  const scrabbleScore: ScrabbleScores = {};
-  for (const [score, characters] of Object.entries(legacyScrabbleScores)) {
-      characters.forEach(character => scrabbleScore[character.toLocaleLowerCase()] = Number(score));
-  }
-  return scrabbleScore;
+  return Object.fromEntries(
+      Object.entries(legacyScrabbleScores)
+          .flatMap(([score, characters]) =>
+              characters.map(character => [character.toLocaleLowerCase(), Number(score)])
+          )
+  );
 }
